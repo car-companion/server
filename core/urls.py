abf9ea474057306c11091dedecd7b9a5424f0777
Mohammadali Azani <ali_azani@yahoo.com>
2024-10-28 00:01:47 +0100

    update the name of main app to core.

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
-from django.conf.urls import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.jwt")),
 ]
