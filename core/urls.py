173f4da03d1edab6ff5fc94b224e8ee86ea708f2
Mohammadali Azani <ali_azani@yahoo.com>
2024-11-03 21:01:20 +0100

    add static urls, when debug is true.

--- core/urls.py
+++ core/urls.py
@@ -1,20 +1,5 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
@@ -35,6 +20,7 @@ auth_patterns = ([
                      path('', include('djoser.urls.jwt')),
                  ], 'auth')
 
+# Core URL patterns
 urlpatterns = [
     path('api/', include([
         path('', include(router.urls)),
@@ -46,4 +32,7 @@ urlpatterns = [
     path('health/', include('health_check.urls')),
 ]
 
-# static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Static URL patterns for development
+if settings.DEBUG:
+    static_patterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns.extend(static_patterns)
