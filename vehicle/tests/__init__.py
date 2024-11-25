25152d8760ab5a2304d82c09e79e81182925ecda
Mohammadali Azani <ali_azani@yahoo.com>
2024-11-25 21:10:00 +0100

    test if pipeline pass or not.

--- vehicle/tests/__init__.py
+++ vehicle/tests/__init__.py
@@ -9,4 +9,5 @@ __all__ = ['ColorAdminTests',
            'ColorModelTests',
            'ManufacturerModelTests',
            'ManufacturerAdminTests',
+           # 'VehicleOwnershipTests'
            ]
