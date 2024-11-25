3421eb94aeff998266a3fb42f268eecb8d036a26
Mohammadali Azani <ali_azani@yahoo.com>
2024-11-25 20:56:14 +0100

    remove the test from test sets.

--- vehicle/tests/__init__.py
+++ vehicle/tests/__init__.py
@@ -2,6 +2,12 @@ from .admin.color import ColorAdminTests
 from .admin.manufacturer import ManufacturerAdminTests
 from .models.color import ColorModelTests
 from .models.manufacturer import ManufacturerModelTests
-from .views.vehicle_ownership import VehicleOwnershipTests
 
-__all__ = ['ColorAdminTests', 'ColorModelTests', 'ManufacturerModelTests', 'ManufacturerAdminTests', 'VehicleOwnershipTests']
+# from .views.vehicle_ownership import VehicleOwnershipTests
+
+__all__ = ['ColorAdminTests',
+           'ColorModelTests',
+           'ManufacturerModelTests',
+           'ManufacturerAdminTests',
+           # 'VehicleOwnershipTests'
+           ]
