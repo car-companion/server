7b872f18802a9509c7f7f65984b24b67900a6bef
Mohammadali Azani <ali_azani@yahoo.com>
2025-01-11 16:45:23 +0100

    add static.

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
@@ -25,3 +27,6 @@ urlpatterns = [
     path('', include('authentication.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
