a0b423e5d4c69fa77f7f54cad00c2aa448cca7e9
Mohammadali Azani <ali_azani@yahoo.com>
2025-01-11 16:29:57 +0100

    optimize imports.

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
